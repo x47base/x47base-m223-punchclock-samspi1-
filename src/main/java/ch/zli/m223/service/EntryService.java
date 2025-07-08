8cb40fdd140d4a05b7490ffb45fa2a5c9c8b91c3
Samuel Spink <dr.greenjet@gmail.com>
2025-07-08 13:49:16 +0200

    initial commit

---
+++ src/main/java/ch/zli/m223/service/EntryService.java
@@ -0,0 +1,36 @@
+package ch.zli.m223.service;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import ch.zli.m223.model.Entry;
+
+@ApplicationScoped
+public class EntryService {
+    @Inject
+    private EntityManager entityManager;
+
+    @Transactional
+    public Entry createEntry(Entry entry) {
+        entityManager.persist(entry);
+        return entry;
+    }
+
+    public List<Entry> findAll() {
+        var query = entityManager.createQuery("FROM Entry", Entry.class);
+        return query.getResultList();
+    }
+
+    public Boolean deleteEntry (int id) {
+        Entry entry = entityManager.find(Entry.class, id);
+        if (entry != null) {
+            entityManager.remove(entry);
+            return true;
+        }
+        return false;
+    }
+}
